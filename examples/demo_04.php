b39ebe85787005a97a63594c94f8db0cb5fe0564
Денис Петрович Сепетов <kafedra@sepetov.ru>
2023-01-05 14:19:54 +0500

    Update htmlSQL filename in examples

--- examples/demo_04.php
+++ examples/demo_04.php
@@ -1,36 +1,35 @@
 <?php
 
-    /*
-    ** htmlSQL - Example 4
-    **
-    ** Shows a advanced query with preg_match
-    */
+/*
+** htmlSQL - Example 4
+**
+** Shows a advanced query with preg_match
+*/
 
-    include_once("../snoopy.class.php");
-    include_once("../htmlsql.class.php");
-    
-    $wsql = new htmlsql();
-    
-    // connect to a URL
-    if (!$wsql->connect('url', 'http://codedump.jonasjohn.de/links.htm')){
-        print 'Error while connecting: ' . $wsql->error;
-        exit;
-    }
-    
-    /* execute a query:
-       
-       This query returns all links of an document that start with http://
-    */
-    if (!$wsql->query('SELECT * FROM a WHERE preg_match("/^http:\/\//", $href)')){
-        print "Query error: " . $wsql->error; 
-        exit;
-    }
+include_once("../snoopy.php");
+include_once("../htmlsql.php");
 
-    // show results:
-    foreach($wsql->fetch_array() as $row){
-    
-        print_r($row);
-        
-    }
-    
-?>
+$wsql = new htmlsql();
+
+// connect to a URL
+if (!$wsql->connect('url', 'http://codedump.jonasjohn.de/links.htm'))
+{
+    print 'Error while connecting: ' . $wsql->error;
+    exit;
+}
+
+/* execute a query:
+
+   This query returns all links of an document that start with https://
+*/
+if (!$wsql->query('SELECT * FROM a WHERE preg_match("/^https:\/\//", $href)'))
+{
+    print "Query error: " . $wsql->error;
+    exit;
+}
+
+// show results:
+foreach($wsql->fetch_array() as $row)
+{
+    print_r($row);
+}
--- examples/demo_12.php
+++ examples/demo_12.php
@@ -1,44 +1,43 @@
 <?php
 
-    /*
-    ** htmlSQL - Example 12
-    **
-    ** Shows how to replace the user agent and the referer with
-    ** custom values
-    */
+/*
+** htmlSQL - Example 12
+**
+** Shows how to replace the user agent and the referer with
+** custom values
+*/
 
-    include_once("../snoopy.class.php");
-    include_once("../htmlsql.class.php");
-    
-    $wsql = new htmlsql();
-    
-    // set a individual agent:
-    $wsql->set_user_agent('MyAgentName/0.9');
-    
-    // set a new referer:
-    $wsql->set_referer('http://www.jonasjohn.de/custom/referer/');
-    
-    
-    // connect to a URL
-    if (!$wsql->connect('url', 'http://codedump.jonasjohn.de/')){
-        print 'Error while connecting: ' . $wsql->error;
-        exit;
-    }
-    
-    /* execute a query:
-       
-       This query returns all links:
-    */
-    if (!$wsql->query('SELECT * FROM a')){
-        print "Query error: " . $wsql->error; 
-        exit;
-    }
+include_once("../snoopy.php");
+include_once("../htmlsql.php");
 
-    // fetch results as array
-    foreach($wsql->fetch_array() as $row){
-    
-        print_r($row);
-        
-    }
-    
-?>
+$wsql = new htmlsql();
+
+// set a individual agent:
+$wsql->set_user_agent('MyAgentName/0.9');
+
+// set a new referer:
+$wsql->set_referer('http://www.jonasjohn.de/custom/referer/');
+
+
+// connect to a URL
+if (!$wsql->connect('url', 'http://codedump.jonasjohn.de/'))
+    {
+    print 'Error while connecting: ' . $wsql->error;
+    exit;
+}
+
+/* execute a query:
+
+   This query returns all links:
+*/
+if (!$wsql->query('SELECT * FROM a'))
+{
+    print "Query error: " . $wsql->error;
+    exit;
+}
+
+// fetch results as array
+foreach($wsql->fetch_array() as $row)
+{
+    print_r($row);
+}
